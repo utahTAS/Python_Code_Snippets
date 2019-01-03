852fbcc256965943b7fb0cfe9a4a2c51be117325
Bcubrich <41648759+Bcubrich@users.noreply.github.com>
2019-01-03 14:12:05 -0700

    Update Add_TK_Quit_Button.py

--- GUI/Add_TK_Quit_Button.py
+++ GUI/Add_TK_Quit_Button.py
@@ -17,5 +17,8 @@ def quit():
 #Create the button. The button text is specified by the text arg. 
 #This one will say "OK"
 button = Button(master, text="OK", command=quit)
-#define button location. In this case the buttons location is definted in a grid. 
+
+
+#define button location. In this case the buttons location is definted in a grid.
+#The 'sticky=W' gives right aligned features. Case use any of 'NSEW'.
 button.grid(row=10, column=2, sticky=W,)
