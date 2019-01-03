49c12e527720c620c4a507eecb61c74b8c83459b
Bcubrich <41648759+Bcubrich@users.noreply.github.com>
2019-01-03 14:09:27 -0700

    Close TK button label "OK"
    Create a tk window that has a close button label "OK"

---
+++ GUI/Add_TK_Quit_Button.py
@@ -0,0 +1,21 @@
+###########################################################
+## Create a tk window that has a close button label "OK" ##
+###########################################################
+
+
+from tkinter import Tk
+from tkinter import *
+
+#start the tk window, make it always on top
+master = Tk()
+master.attributes("-topmost", True)      #makes the dialog appear on top
+
+#function to close the window when the button is pressed
+def quit():
+    master.destroy()
+
+#Create the button. The button text is specified by the text arg. 
+#This one will say "OK"
+button = Button(master, text="OK", command=quit)
+#define button location. In this case the buttons location is definted in a grid. 
+button.grid(row=10, column=2, sticky=W,)
